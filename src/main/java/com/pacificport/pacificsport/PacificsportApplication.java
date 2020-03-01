a847532210091755fd93b262c8c2f27e8a0794d8
Ricardo Carballo <rcarbaleq2@gmail.com>
2020-02-29 17:09:21 -0800

    Initial commit

---
+++ src/main/java/com/pacificport/pacificsport/PacificsportApplication.java
@@ -0,0 +1,13 @@
+package com.pacificport.pacificsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacificsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacificsportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacificport/pacificsport/PacificsportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacificport.pacificsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacificsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
