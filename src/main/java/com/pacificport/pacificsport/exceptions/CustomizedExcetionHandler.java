2e181dce24892b9145f732bb538bddebba0df4a1
Ricardo Carballo <rcarbaleq2@gmail.com>
2020-03-03 23:17:19 -0800

    Create a custum exception class for handling all exception and user not found exeption

---
+++ src/main/java/com/pacificport/pacificsport/exceptions/CustomizedExcetionHandler.java
@@ -0,0 +1,39 @@
+package com.pacificport.pacificsport.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+// this is applicable to all the other controllers
+@ControllerAdvice
+@RestController
+public class CustomizedExcetionHandler extends ResponseEntityExceptionHandler {
+
+
+    // this method was copied from the ResponseEntityExceptionHandler class
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleALLException(Exception ex, WebRequest request) throws Exception {
+
+        // when an exception happens create a new instance of our custom bean using an ExceptionResponse
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) throws Exception {
+
+        // when an exception happens create a new instance of our custom bean using an ExceptionResponse
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(response, HttpStatus.NOT_FOUND);
+
+    }
+}
