0d638d4642475562c62c2be71cef5083d0b14b67
Ricardo Carballo <rcarbaleq2@gmail.com>
2020-03-05 04:02:36 -0800

    add Thymeleaf support in pom.xml - add orderform.html

--- src/main/java/com/pacificport/pacificsport/controller/OrderController.java
+++ src/main/java/com/pacificport/pacificsport/controller/OrderController.java
@@ -1,16 +1,21 @@
 package com.pacificport.pacificsport.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/orders")
+import java.util.Date;
+
+@Controller
 public class OrderController {
 
-    @GetMapping("/orderform")
-    public String getOrderForm(){
-        return "Get Order Form";
+    @RequestMapping("/orderform")
+    public String getOrderForm(Model theModel){
+
+        theModel.addAttribute("theDate", new Date());
+        return "orderform";
     }
 
 }
