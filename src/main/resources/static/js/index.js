70c387f63e86956120b5fc3d8985b2a957da2643
Ricardo Carballo <rcarbaleq2@gmail.com>
2020-03-08 00:02:28 -0800

    Orderfor duplicates group-input on ENTER pressed

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,106 @@
+let COUNT = 1;
+enterPressed();
+
+
+function enterPressed(){
+  let inputGroup = document.getElementById("main-input-group");
+
+  inputGroup.addEventListener("keyup", () => {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      console.log("Enter Key Pressed");
+
+      addNewInputToInputGroup();
+  
+    }
+  });
+}
+
+// <div class="input-group" id="main-input-group">
+  // <div class="input-group-prepend">
+  //     <span class="input-group-text" id="">Item</span>
+  // </div>
+  // <input type="text" class="form-control">
+  // <input type="text" class="form-control">
+  // <input type="text" class="form-control">
+  // <input type="text" class="form-control">
+// </div>"
+
+function addNewInputToInputGroup(){
+
+  // get main input group
+  let inputGroupContainer = document.getElementById("main-input-group");
+  
+  let inputGroup = document.createElement("div");
+  inputGroup.classList.add("input-group");
+
+  // crearte the prepend divs
+  let prependDiv = document.createElement("div");
+  let span = document.createElement("span");
+
+    //prepend gets the span
+  prependDiv.classList.add("input-group-prepend");
+  prependDiv.classList.add("mb-1");
+  span.classList.add("input-group-text");
+
+  COUNT++;
+  span.innerHTML = COUNT;
+
+  prependDiv.appendChild(span);
+
+  //input group gets the prepend
+  inputGroup.appendChild(prependDiv);
+
+
+
+  // required inputs for item, color, size, quantity, price
+
+  // item
+  let itemInput = document.createElement("input");
+  itemInput.classList.add("form-control");
+  itemInput.classList.add("mb-1");
+  itemInput.setAttribute("type", "text");
+  itemInput.placeholder = "item"
+
+  //color
+  let colorInput = document.createElement("input");
+  colorInput.classList.add("form-control");
+  colorInput.classList.add("mb-1");
+  colorInput.setAttribute("type", "text");
+  colorInput.placeholder = "color"
+
+  //size
+  let sizeInput = document.createElement("input");
+  sizeInput.classList.add("form-control");
+  sizeInput.classList.add("mb-1");
+  sizeInput.setAttribute("type", "text");
+  sizeInput.placeholder = "size"
+
+  // quantity
+  let quantityInput = document.createElement("input");
+  quantityInput.classList.add("form-control");
+  quantityInput.classList.add("mb-1");
+  quantityInput.setAttribute("type", "text");
+  quantityInput.placeholder = "size"
+  
+
+  //price
+  let priceInput = document.createElement("input");
+  priceInput.classList.add("form-control");
+  priceInput.classList.add("mb-1");
+  priceInput.setAttribute("type", "text");
+  priceInput.placeholder = "$"
+  
+
+
+
+  inputGroup.appendChild(itemInput);
+  inputGroup.appendChild(colorInput);
+  inputGroup.appendChild(quantityInput);
+  inputGroup.appendChild(priceInput);
+  
+  inputGroupContainer.appendChild(inputGroup);
+}
